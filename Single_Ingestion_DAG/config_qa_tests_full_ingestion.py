9e26b947a39d08faaa07ef7c8d6306cafb82fec6
Rajashekar Allala <50412459+RajashekarAllala@users.noreply.github.com>
2025-12-13 18:51:44 +0530

    Rename DAG ID to 'config_qa_tests_full_ingestion'

--- Single_Ingestion_DAG/config_qa_tests_full_ingestion.py
+++ Single_Ingestion_DAG/config_qa_tests_full_ingestion.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import json
 
 with DAG(
-    dag_id="config_qa_tests_with_exceptions",
+    dag_id="config_qa_tests_full_ingestion",
     start_date=datetime(2025, 1, 1),
     schedule_interval=None,
     catchup=False,
