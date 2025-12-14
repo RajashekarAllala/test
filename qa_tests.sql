424ef4bbfab651447dbd64985bc2682c3ee47803
Rajashekar Allala <50412459+RajashekarAllala@users.noreply.github.com>
2025-12-14 10:37:32 +0530

    Update qa_tests.sql

--- qa_tests.sql
+++ qa_tests.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS `{{qa_project}}.{{qa_dataset}}.qa_execution_log` (
-  dag_name STRING,
-  dag_run_datetime TIMESTAMP,
-  table_name STRING,
-  load_type STRING,
-  task_name STRING,
-  task_status STRING,
-  qa_status STRING,
-  failed_task_name STRING,
-  failure_reason STRING
+CREATE TABLE IF NOT EXISTS `<Project_ID>.<DataSet>.qa_test_log` (
+  dag_name             STRING,
+  dag_run_datetime     TIMESTAMP,
+  table_name           STRING,
+  load_type            STRING,
+  task_name            STRING,
+  task_status          STRING,
+  qa_status            STRING,
+  failed_task_name     STRING,
+  failure_reason       STRING
 )
 PARTITION BY DATE(dag_run_datetime)
 CLUSTER BY table_name, qa_status;
