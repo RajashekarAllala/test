76ae7000097d0a961342e2f0a9a50305afbde604
Rajashekar Allala <50412459+RajashekarAllala@users.noreply.github.com>
2025-12-14 10:46:11 +0530

    Update target_cdc.sql

--- target_cdc.sql
+++ target_cdc.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS `{{project_id}}.{{target_cdc_dataset}}.target_cdc` (
+CREATE TABLE IF NOT EXISTS `<Project_ID>.<DataSet>.ODP_ACCOUNTS_CDC` (
   account_id        STRING NOT NULL,
   balance           NUMERIC NOT NULL,
   as_of_date        DATE NOT NULL,
   rec_load_ts       TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
   as_of_date_time   TIMESTAMP NOT NULL,
-  row_hash          STRING NOT NULL,
   start_date        DATE NOT NULL,
   end_date          DATE NOT NULL,
+  row_hash          STRING NOT NULL,
   is_current        BOOL NOT NULL,
   record_check_ts   TIMESTAMP     -- Last reconciliation timestamp
 )
