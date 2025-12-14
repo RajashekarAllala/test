ae0f9abb77b2cd252e92dc2041b9411e3677182f
Rajashekar Allala <50412459+RajashekarAllala@users.noreply.github.com>
2025-12-14 09:11:25 +0530

    Update qa_tests.sql

--- qa_tests.sql
+++ qa_tests.sql
@@ -1,26 +1,13 @@
 CREATE TABLE IF NOT EXISTS `{{qa_project}}.{{qa_dataset}}.qa_execution_log` (
-  dag_name              STRING NOT NULL,     -- Ingestion DAG name
-  run_id                STRING NOT NULL,     -- DAG run id
-
-  table_name            STRING NOT NULL,     -- Logical table name
-  load_type             STRING NOT NULL,     -- RELOAD / INCREMENTAL
-
-  qa_status             STRING NOT NULL,     -- PASS / FAIL
-
-  failed_task_name      STRING,              -- Task that caused QA failure
-  failure_reason        STRING,              -- Human-readable reason
-
-  job_start_time        TIMESTAMP,           -- From ingestion DAG
-  job_end_time          TIMESTAMP,
-
-  source_count          INT64,
-  target_count          INT64,
-
-  new_records           INT64,
-  unchanged_records     INT64,
-  changed_records       INT64,
-
-  created_timestamp     TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+  dag_name STRING,
+  dag_run_datetime TIMESTAMP,
+  table_name STRING,
+  load_type STRING,
+  task_name STRING,
+  task_status STRING,
+  qa_status STRING,
+  failed_task_name STRING,
+  failure_reason STRING
 )
-PARTITION BY DATE(created_timestamp)
+PARTITION BY DATE(dag_run_datetime)
 CLUSTER BY table_name, qa_status;
