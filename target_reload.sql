e4286fd73e8c449ba224c6e99c64820a2e064017
Rajashekar Allala <50412459+RajashekarAllala@users.noreply.github.com>
2025-12-14 10:43:13 +0530

    Update target_reload.sql

--- target_reload.sql
+++ target_reload.sql
@@ -3,6 +3,5 @@ CREATE TABLE IF NOT EXISTS `<Project_ID>.<DataSet>.ODP_ACCOUNTS` (
   balance           NUMERIC NOT NULL,
   as_of_date        DATE NOT NULL,
   rec_load_ts       TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-  as_of_date_time   TIMESTAMP NOT NULL,
-  record_check_ts   TIMESTAMP
+  as_of_date_time   TIMESTAMP NOT NULL
 );
