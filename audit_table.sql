0f376b1bc29beded4aee0a28a4c362028d5df47f
Rajashekar Allala <50412459+RajashekarAllala@users.noreply.github.com>
2025-12-14 09:12:06 +0530

    Update audit_table.sql

--- audit_table.sql
+++ audit_table.sql
@@ -1,30 +1,16 @@
 CREATE TABLE IF NOT EXISTS `{{project_id}}.{{audit_dataset}}.ingestion_audit_log` (
-  dag_name               STRING NOT NULL,
-  run_id                 STRING NOT NULL,
-  table_name             STRING NOT NULL,
-  load_type              STRING NOT NULL,
-
-  task_name              STRING NOT NULL,      -- which task
-  task_status            STRING NOT NULL,      -- SUCCESS / FAILED
-
-  job_start_time         TIMESTAMP,
-  job_end_time           TIMESTAMP,
-
-  job_status             STRING,               -- SUCCESS / FAILED
-  audit_status           STRING,               -- PASS / FAIL
-
-  source_count           INT64,
-  target_count           INT64,
-  mismatch               INT64,
-
-  new_records            INT64,
-  unchanged_records      INT64,
-  changed_records        INT64,
-
-  error_message          STRING,               -- exception text
-  error_type             STRING,               -- FOOTER / DQ / LOAD / MERGE
-
-  created_timestamp      TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+  dag_name STRING,
+  table_name STRING,
+  task_id STRING,
+  task_name STRING,
+  task_start_datetime TIMESTAMP,
+  task_end_datetime TIMESTAMP,
+  duration_seconds INT64,
+  task_status STRING,
+  audit_status STRING,
+  source_count INT64,
+  target_count INT64,
+  error_message STRING
 )
-PARTITION BY DATE(created_timestamp)
-CLUSTER BY table_name, load_type;
+PARTITION BY DATE(task_start_datetime)
+CLUSTER BY table_name, task_name;
